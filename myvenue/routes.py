e07a3a85cb9e18643010e446b8f833112ae561a8
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-04-12 21:01:42 +0100

    basic stylings and layout created

--- myvenue/routes.py
+++ myvenue/routes.py
@@ -5,3 +5,8 @@ from myvenue import app, db
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/listings")
+def listings():
+    return render_template("listings.html")
