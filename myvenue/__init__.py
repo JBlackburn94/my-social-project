362a597da5353d7ea316b50d2779bca4df4af79f
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-04-27 14:32:49 +0100

    installed postgres and created the database for my models

--- myvenue/__init__.py
+++ myvenue/__init__.py
@@ -7,7 +7,7 @@ if os.environ.get("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///event"
 
 db = SQLAlchemy(app)
 
